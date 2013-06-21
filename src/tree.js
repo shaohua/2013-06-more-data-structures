5652cbbca2c33c0a67f0af7fbf7bad8205adb1fa
Hack Reactor Students <github@hackreactor.com>
2013-06-20 17:42:38 -0700

    Code cleanup for .contains

--- src/tree.js
+++ src/tree.js
@@ -16,32 +16,22 @@ treeMethods.addChild = function(value){
 };
 
 treeMethods.contains = function(input){ //input should be 'value', not a tree
-  // debugger;
-  var that = this;
   var found = false;
-  var count = 0;
   var walker = function(input_tree){
-    count++;
-    console.log(count, input_tree, input_tree['value'], input, found);
-
-    if(input_tree['value'] === input){
+    if(input_tree.value === input){
       found = true;
       return true;
     }
 
-    console.log(count, input_tree, input_tree['value'], input, found);
-
     if(input_tree.children !== undefined){
-      _.each(input_tree.children, function(item, key, collection){
-        walker(item);
+      _.each(input_tree.children, function(value, index, collection){
+        walker(value);
       });
     }
-    // debugger;
   };
 
-  walker(that);//passing a tree, not a value
+  walker(this);
 
-  // debugger;
   return found;
 
 };
