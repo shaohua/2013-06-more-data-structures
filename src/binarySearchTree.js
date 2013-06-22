643b4830f0fbd21ddd68f5ff997fa5b96ee1e663
Hack Reactor Students <github@hackreactor.com>
2013-06-22 15:57:39 -0700

    Attaching _walker() to prototype

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -4,6 +4,44 @@ var BinarySearchTree = function(value){
   this.right = undefined;
 };
 
+BinarySearchTree.prototype._walker = function(node, input, containFlag){
+  containFlag = containFlag || false;
+
+  if(node.value === undefined || node.value === null) {
+    node.value = input;
+    console.log('inserting', input, node.value);
+  } else {
+    if(node.value === input){
+      console.log("This value is already in the tree!");
+      return;
+    } else {
+
+      if(node.value > input){
+
+        if(node.right === null || node.right === undefined){
+          node.right = new BinarySearchTree(input);
+        } else {
+          walker(node.right, input);
+        }
+
+      } else if(node.value < input) {
+
+          if(node.left === null || node.left === undefined){
+            node.left = new BinarySearchTree(input);
+          } else {
+            walker(node.left, input);
+          }
+
+      } else {
+
+        console.log("This is a weird case, fix me!");
+
+      }
+
+    }
+  }
+};
+
 BinarySearchTree.prototype.insert = function(input){
 // Step 1: Go to node and ask, "Am I bigger or smaller than you?"
 //         * If smaller, go to left node
@@ -13,46 +51,7 @@ BinarySearchTree.prototype.insert = function(input){
 //           * If right node is undefined, become the right node
 //           * If right node is present, go back to Step 1
 
-  var walker = function(node){
-
-    if(node.value === undefined || node.value === null) {
-      node.value = input;
-      console.log('inserting', input, node.value);
-    } else {
-      if(node.value === input){
-        console.log("This value is already in the tree!");
-        return;
-      } else {
-
-        if(node.value > input){
-
-          if(node.right === null || node.right === undefined){
-            node.right = new BinarySearchTree(input);
-          } else {
-            walker(node.right);
-          }
-
-        } else if(node.value < input) {
-
-            if(node.left === null || node.left === undefined){
-              node.left = new BinarySearchTree(input);
-            } else {
-              walker(node.left);
-            }
-
-        } else {
-
-          console.log("This is a weird case, fix me!");
-
-        }
-
-      }
-    }
-
-  };
-
-
-  walker(this);
+  this._walker(this, input);
 
 };
 
