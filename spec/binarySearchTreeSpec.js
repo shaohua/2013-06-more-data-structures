4da99c12c76557add6c717a3828a07a7c9ee472a
Hack Reactor Students <github@hackreactor.com>
2013-06-22 15:51:04 -0700

    BinarySearch, insert working

--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -2,13 +2,18 @@ describe("binarySearchTree", function() {
   var binarySearchTree;
 
   beforeEach(function() {
-    binarySearchTree = makeBinarySearchTree();
+    binarySearchTree = new BinarySearchTree();
   });
 
   it("should have methods named 'insert', 'contains', and 'depthFirstLog", function() {
-    expect(binarySearchTree.children).toEqual(jasmine.any(Function));
+    expect(binarySearchTree.insert).toEqual(jasmine.any(Function));
     expect(binarySearchTree.contains).toEqual(jasmine.any(Function));
     expect(binarySearchTree.depthFirstLog).toEqual(jasmine.any(Function));
+  });
+
+  it("should insert one value to the search tree", function() {
+    binarySearchTree.insert(5);
+    expect(binarySearchTree.value).toEqual(5);
   });
   // add more tests here to test the functionality of binarySearchTree
 });
--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,2 +1,74 @@
-var makeBinarySearchTree = function(){
+var BinarySearchTree = function(value){
+  this.value = value;
+  this.left = undefined;
+  this.right = undefined;
+};
+
+BinarySearchTree.prototype.insert = function(input){
+// Step 1: Go to node and ask, "Am I bigger or smaller than you?"
+//         * If smaller, go to left node
+//           * If left node is undefined, become the left node
+//           * If left node is present, go back to Step 1
+//         * If bigger, go to right node
+//           * If right node is undefined, become the right node
+//           * If right node is present, go back to Step 1
+
+  var walker = function(node){
+
+    if(node.value === undefined || node.value === null) {
+      node.value = input;
+      console.log('inserting', input, node.value);
+    } else {
+      if(node.value === input){
+        console.log("This value is already in the tree!");
+        return;
+      } else {
+
+        if(node.value > input){
+
+          if(node.right === null || node.right === undefined){
+            node.right = new BinarySearchTree(input);
+          } else {
+            walker(node.right);
+          }
+
+        } else if(node.value < input) {
+
+            if(node.left === null || node.left === undefined){
+              node.left = new BinarySearchTree(input);
+            } else {
+              walker(node.left);
+            }
+
+        } else {
+
+          console.log("This is a weird case, fix me!");
+
+        }
+
+      }
+    }
+
+  };
+
+
+  walker(this);
+
+};
+
+BinarySearchTree.prototype.contains = function (query) {
+// found = false;
+// walker(node){ Go to node, compare 'query' to node.value,
+//         if true, found = true, return
+//         if query > node.value, current_node = node.right
+//         else if query < node.value, current_node = node.left
+//         walker(current_node)
+// }
+// walker(this)
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(callback){
+// Step 1: Go to node, callback(node)
+//         While node has chi
+
 };
